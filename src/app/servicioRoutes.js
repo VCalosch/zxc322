071aafdf50c7973af67134ea6175184aeaa5db38
YOUR_USERNAME <carlos.villanueva@alumnos.uneatlantico.es>
2018-09-13 19:31:13 +0200

    Problemas Mostrando Marcadores

--- src/app/servicioRoutes.js
+++ src/app/servicioRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express'); //lamar a express	
 const routServicio = express.Router(); //crear un objeto de tipo express con nombre routPlaya
 const ServicioModel = require('../app/models/ServicioPlaya'); //llamar al modelo del base de datos
+const UbicarServiciosModel = require('../app/models/UbicarServicios');
 const modelPlaya = require('../app/models/playa');
 
 
@@ -10,24 +11,42 @@ routServicio.get('/ss/:id', (req,res) =>{
 	modelPlaya.findById({_id:id}, (err, playa) =>{
 		if(err) throw err;
 		  ServicioModel.find({ }, (err,servicio) =>{
-		  	if (err) throw err;
+			if (err) throw err;
+			UbicarServiciosModel.find({ }, (err,UbicarServicio) =>{
+			if (err) throw err;
 		  	 else{
 		  	 	res.render('servicioPlaya_v', {
 					Playas: playa,
 					Servicio: servicio,
-				    isLoggedIn: req.isAuthenticated()});
-		  	 }
+					UbicarServicio: UbicarServicio,
+					isLoggedIn: req.isAuthenticated()});
+				   }
+		  	 })
 		  });
 	})
 });
+
+routServicio.get('/ss/:id', (req,res) =>{
+	let id = req.params.id;
+	UbicarServiciosModel.find((err, playa) =>{
+			if (err) throw err;
+		  	 else{
+		  	 	res.render('servicioPlaya_v', {
+					UbicarServicio: UbicarServicio
+					});
+				   }
+		  	 })
+		  });
+	
+
 
 routServicio.post('/ss', isLoggedIn, (req, res) => {
 	let body = req.body;
 	body.status = false;
 
-	ServicioModel.create(body, (err, Playa) => {
+	UbicarServiciosModel.create(body, (err, Playa) => {
 		if (err) throw err;
-		res.redirect('/ss');
+		res.redirect('back');
 	});
 });
 
