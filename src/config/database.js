e72c4e7056cd68473a2745dc1d996a7d42d89039
Carlos José Villanueva Portillo <carlos.villanueva@alumnos.uneatlantico.es>
2018-10-26 16:10:11 +0200

    Primera Version 2

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,4 @@
 module.exports = {
-	'url': 'mongodb://localhost/plsNode',
-	'port': '27018'
+	'url': 'mongodb://playas:playas@localhost/plsNode',
 }
 
-// module.exports = {
-// 	'url': 'mongodb://playas:playas@localhost/plsNode',
-// }
